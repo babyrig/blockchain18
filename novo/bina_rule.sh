152621a4fb0b85569b5f258bce2566159a70e5d2
Alecsandro <38074298+babyrig@users.noreply.github.com>
2018-12-14 14:57:06 -0200

    update

--- novo/bina_rule.sh
+++ novo/bina_rule.sh
@@ -16,13 +16,8 @@ do
 #       $IPT -A INPUT -i eth0 -s $ip -j droplist
 #        $IPT -A droplist -i eth0 -p tcp --dport 8884 -s $ip -j DROP
         $IPT -A droplist -i eth0 -s $ip -j DROP
-<<<<<<< HEAD
-
-=======
->>>>>>> ffa2e0832774461b3af1d85c7f0af27096dc17d6
 done < "$_input"
 # Drop it
 $IPT -I INPUT -j droplist
 $IPT -I OUTPUT -j droplist
 $IPT -I FORWARD -j droplist
-
